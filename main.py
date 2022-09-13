c47e7f428f2557014b58c65eee5487c4cd61af8c
JamerM <jmartinez6428@misena.edu.co>
2022-09-12 21:49:25 -0500

    ejercicios en codigo

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-#esto es un  comentario
+#esto es un  comentario de linea
+#ejercicios de varaibles
+"""mensaje = "hola mundo estoy aprendiendo python"
+print(mensaje)"""
+
+nombre = "Jamer"
+apellido = "Martinez"
+ciudad = "Barranquilla"
+celular = 320000011
+
+#print(nombre + " " + apellido + " " + ciudad + " " + str(celular))
+#print("hola %s de %s " % (nombre,apellido))
+
+#print(f"hola {nombre} {apellido} de la ciudad {ciudad} con celular {celular}")
+
+#leer datos desde teclado
+nombre = input("digita tu nombre: ")
+print(nombre)
+
